32acb76592137a7f6cb23045c308e9e303d53418
RachelKolk <rach.m.obrien@gmail.com>
2019-01-18 13:21:20 -0500

    done with tab/tabs working on cards

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,18 +4,18 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      
-    // activeTab = e => {
-    //   if ({mappedTabs.tab} === {props.selectedTab} ? className= "tab active-tab" : className="tab");
-    // }
+
+    let className = "";
+    if (props.tab === props.selected ? className= "tab active-tab" : className="tab");
+    
 
   return (
     <div
-      className={''}
+      className={className}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectTabHandler(props.tab)
       }}
+      // onClick={props.selectTabHandler(props.tab)}       
     >
       {props.tab.toUpperCase()}
     </div>
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,14 +8,14 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-          {props.tabs.map(mappedTabs =>
+          {props.tabs.map(mappedTab =>
             <Tab
-              // allTabs={mappedTabs}
-              // selectTabHandler={props.selectTabHandler}
-              // selectedTab={props.selectedTab}
-              // tab={props.tab}
+              tab={mappedTab}
+              selectTabHandler={props.selectTabHandler}
+              selectedTab={props.selectedTab}
               
-              tab={mappedTabs}
+              
+        
             />
           )}
       </div>
